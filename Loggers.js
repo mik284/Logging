74196283d0d1b1443a01d63bcd1b40bd140e05e8
mik284 <mikeykamau222@gmail.com>
2022-12-08 19:33:59 +0300

    Added loggers for dev and production env

--- Loggers.js
+++ Loggers.js
@@ -1,1 +1,62 @@
 const winston = require("winston")
+const {
+    createLogger,
+    transports,
+    format
+} = winston
+
+const {
+    combine,
+    colorize,
+    timestamp,
+    printf,
+    errors
+} = format
+
+const logFormat = printf(({
+    level,
+    message,
+    stack,
+    timestamp
+}) => {
+    return `[${timestamp}]: ${level} ${ stack || message}`
+})
+
+
+exports = {
+    devLogger: () => {
+        return createLogger({
+            format: combine(
+                colorize(),
+                timestamp({
+                    format: "YYYY-MM-DD HH:mm:ss"
+                }),
+                errors({
+                    stack: true
+                }),
+                logFormat
+            ),
+            transports: [new transports.Console(),
+                new transports.File({
+                    filename: "Logging/logs.txt"
+                })
+            ]
+        })
+    },
+    prodLogger: () => {
+        return createLogger({
+            format: combine(
+                timestamp({
+                    format: "YYYY-MM-DD HH:mm:ss"
+                }),
+                logFormat
+            ),
+            transports: [
+                new transports.Console(),
+                new transports.File({filename: "Logging/logs.txt"})
+            ]
+
+        })
+    }
+}
+
--- logger.js
+++ logger.js
@@ -1,34 +1,52 @@
 const winston = require('winston')
-const {
-    createLogger,
-    transports,
-    format
-} = winston
+// const {
+//     createLogger,
+//     transports,
+//     format
+// } = winston
 
-const { combine, colorize, simple, json, prettyPrint, timestamp, printf} = format
-const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} ${level}: ${message}`;
-});
+// const { combine, colorize, simple, json, prettyPrint, timestamp, printf} = format
+// const logFormat = printf(({ level, message, label, timestamp }) => {
+//   return `${timestamp} ${level}: ${message}`;
+// });
 
-const logger = createLogger({
-    format: combine(
-        colorize(),
-        // prettyPrint(),
-        // json()
-        timestamp({format: "YYYY-MM-DD  HH:mm:ss"}),
-        simple(),
-        logFormat
-    ),
-    transports: [
-        new transports.Console()
-    ]
-})
+const dotenv = require('dotenv')
+const { devLogger, prodLogger } = require('./Loggers')
+dotenv.config()
 
-logger.error("my firset error with winston")
-logger.info("my firset info with winston")
-logger.warn("my firset warn with winston")
-logger.silly("my first silly with winston")
+console.log(process.env.NODE_ENV);
+const env = process.env.NODE_ENV
+let logger = null
 
+
+
+if(env === "Production"){
+    logger= prodLogger()
+} else{
+    logger = devLogger()
+}
+
+// const logger = createLogger({
+//     format: combine(
+//         colorize(),
+//         // prettyPrint(),
+//         // json()
+//         timestamp({format: "YYYY-MM-DD  HH:mm:ss"}),
+//         simple(),
+//         logFormat
+//     ),
+//     transports: [
+//         new transports.Console()
+//     ]
+// })
+
+//logger.error("my first error with winston")
+//  logger.info("my first info with winston")
+//  logger.warn("my first warn with winston")
+//  logger.silly("my first silly with winston")
+
+
+module.export = logger
 
 
 
