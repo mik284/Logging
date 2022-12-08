156a3810b0b1c0acec84a07a884e559b8eb19745
mik284 <mikeykamau222@gmail.com>
2022-12-08 09:54:15 +0300

    Added my 1st error with winston

---
+++ logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston')
+const {
+    createLogger,
+    transports,
+    format
+} = winston
+
+
+const logger = createLogger({
+    format: format.simple(),
+    transports: [
+        new transports.Console()
+    ]
+})
+
+logger.error("my firset error with winston")
+
+
+
+// Logging levels in winston conform to the severity ordering specified by 
+// RFC5424: severity of all levels is assumed to be numerically ascending 
+// from most important to least important
+
+// const levels = {
+//     error: 0,
+//     warn: 1,
+//     info: 2,
+//     http: 3,
+//     verbose: 4,
+//     debug: 5,
+//     silly: 6
+// };
